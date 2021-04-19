f50e8606a6b518444ed9ae7ece6fb022be7fb62c
Simon West <43855704+simonwestuk@users.noreply.github.com>
2021-04-19 10:30:10 +0100

    Initial commit

---
+++ api.js
@@ -0,0 +1,42 @@
+function getAPIData()
+{
+    var xhttp = new XMLHttpRequest();
+
+    var searchValue = document.getElementById('search-text').value;
+
+    console.log(searchValue);
+
+    var url = 'https://www.thesportsdb.com/api/v1/json/1/searchevents.php?e=' + searchValue;
+
+    console.log(url);
+
+    xhttp.open('GET', url);
+
+    xhttp.send();
+
+    xhttp.onreadystatechange = function()
+    {
+        if (this.readyState == 4 && this.status == 200)
+        {
+
+            data = JSON.parse(this.responseText);
+
+            console.log(data);
+
+            document.getElementById('search-text').value = null;
+
+            for (var i in data.event)
+            {
+                var node = document.createElement("H2");
+                var textnode = document.createTextNode(data.event[i].strEvent + ' ' + data.event[i].dateEvent + ' ' + data.event[i].strTime);
+                node.appendChild(textnode);
+                document.getElementById("results").appendChild(node);
+            }
+
+
+
+        }
+    }
+}
+
+
